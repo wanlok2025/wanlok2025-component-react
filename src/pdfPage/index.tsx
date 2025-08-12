2e77a3784f144de92d682872250cf8615faf2d80
Robert Wan <wantunglok@gmail.com>
2025-08-12 20:52:20 +1000

    add mui charts

--- src/pdfPage/index.tsx
+++ src/pdfPage/index.tsx
@@ -1,17 +1,17 @@
 import { usePDFPage } from "./usePDFPage";
+import { BarChart } from "@mui/x-charts";
 
 export const PDFPage = () => {
   const { rows } = usePDFPage();
-
   return (
     <>
       <div>
         {rows.map((row, i) => {
-          // if (row.type === "barchart") {
-          //   return <BarChart key={`pdf-${i}`} {...JSON.parse(row.value)} barLabel="value" />;
-          // } else {
-          return <div key={`pdf-${i}`}>{row.value}</div>;
-          // }
+          if (row.type === "barchart") {
+            return <BarChart key={`pdf-${i}`} {...JSON.parse(row.value)} barLabel="value" />;
+          } else {
+            return <div key={`pdf-${i}`}>{row.value}</div>;
+          }
         })}
       </div>
     </>
