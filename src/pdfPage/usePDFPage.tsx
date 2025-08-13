43f926e621b81875bae469dcc6caebc101dcf58d
Robert Wan <wantunglok@gmail.com>
2025-08-13 22:45:31 +1000

    add firestore support

--- src/pdfPage/usePDFPage.tsx
+++ src/pdfPage/usePDFPage.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 export interface Row {
   type: string;
@@ -8,5 +11,20 @@ export interface Row {
 export const usePDFPage = () => {
   const location = useLocation();
   const rows = new URLSearchParams(location.search).get("rows");
+  // const { id } = useParams();
+  // const [snapshot, setSnapshot] = useState<Snapshot>();
+
+  useEffect(() => {
+    fetchSnapshot("b1CuqOWKEYir19gGOcrP");
+  }, []);
+
+  const fetchSnapshot = async (id: string) => {
+    const docRef = doc(db, "snapshots", id);
+
+    console.log((await getDoc(docRef)).data());
+
+    // setSnapshot((await getDoc(docRef)).data() as Snapshot);
+  };
+
   return { rows: rows ? (JSON.parse(rows) as Row[]) : [] };
 };
