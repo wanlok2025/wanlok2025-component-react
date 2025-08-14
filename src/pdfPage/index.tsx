41a04ff7be8161b1403a2529ddbc69142576b1d2
Robert Wan <wantunglok@gmail.com>
2025-08-14 23:16:12 +1000

    add id to div

--- src/pdfPage/index.tsx
+++ src/pdfPage/index.tsx
@@ -4,16 +4,14 @@ import { BarChart } from "@mui/x-charts";
 export const PDFPage = () => {
   const { snapshot } = usePDFPage();
   return (
-    <>
-      <div>
-        {snapshot?.rows.map((row, i) => {
-          if (row.type === "barchart") {
-            return <BarChart skipAnimation key={`pdf-${i}`} {...JSON.parse(row.value)} barLabel="value" />;
-          } else {
-            return <div key={`pdf-${i}`}>{row.value}</div>;
-          }
-        })}
-      </div>
-    </>
+    <div id="dummy">
+      {snapshot?.rows.map((row, i) => {
+        if (row.type === "barchart") {
+          return <BarChart skipAnimation key={`pdf-${i}`} {...JSON.parse(row.value)} barLabel="value" />;
+        } else {
+          return <div key={`pdf-${i}`}>{row.value}</div>;
+        }
+      })}
+    </div>
   );
 };
