c4e18b527117520c605250d900b8ebe3810469ad
Robert Wan <wantunglok@gmail.com>
2025-08-16 10:06:22 +1000

    add test styles

--- src/pdfPage/index.tsx
+++ src/pdfPage/index.tsx
@@ -1,10 +1,11 @@
 import { usePDFPage } from "./usePDFPage";
 import { BarChart } from "@mui/x-charts";
+import styles from "./pdfPage.module.css";
 
 export const PDFPage = () => {
   const { snapshot } = usePDFPage();
   return (
-    <div id="dummy">
+    <div className={styles.dummy}>
       {snapshot?.rows.map((row, i) => {
         if (row.type === "barchart") {
           return <BarChart skipAnimation key={`pdf-${i}`} {...JSON.parse(row.value)} barLabel="value" />;
